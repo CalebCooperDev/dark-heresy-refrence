573b9b6ecae590ef17ffb433baedac90540f04b6
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2021-11-27 13:53:43 -0600

    trying to get navigation working

--- DarkHeresy.js
+++ DarkHeresy.js
@@ -3,4 +3,19 @@ function loadDoc(fileName) {
   $("#content").load(fileName);
 }
 
+function navClickEvent() {
 
+  let container = document.querySelector('.navContainer');
+//I tried to use jquery for this but it didn't work. perhaps something to try another time
+  container.addEventListener('click', function (e) {
+    if (e.target != e.currentTarget) {
+      e.preventDefault();
+      let data = e.target.getAttribute('href');
+      history.pushState(data, null, data);
+
+    }
+    e.stopPropagation();
+  }, false);
+
+}
+window.onload = navClickEvent;
